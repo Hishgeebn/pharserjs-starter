4330f6b7377fa73c64c1fa3ed69e74cd30f7d80b
Hishgeebn <khishithug@gmail.com>
2025-08-03 13:43:39 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800, //urgun
+    height: 600, // undur
+    scene: {
+        // door bga function uud
+        preload: preload,
+        create: create,
+    },
+}
+
+const game = new Phaser.Game(config)
+
+function preload() {
+    // map.json, bg.png g ajiluulah
+    this.load.tilemapTiledJSON("mapkey", "./map.json")
+    this.load.image("tiles", "assets/bg.png")
+}
+
+function create() {
+    // map.json oos map uusgeh
+    const map = this.make.tilemap({ key: "mapkey" })
+
+    // map.json -> tilesets -> name == MyTileset baih yostoi
+    const tileset = map.addTilesetImage("MyTileset", "tiles")
+
+    // map.json -> tilesets -> name == MyTileset baih yostoi
+    const layer = map.createLayer("background_layer_1", tileset, 0, 0)
+}
